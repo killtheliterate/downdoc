08e330b223fa50daa6dfb69d0a4162e5112f6a7f
talon.poole@gmail.com <talon.poole@gmail.com>
2015-08-14 17:57:56 -0600

    more sensible index.js

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-var fs = require('io.filesystem')(require('fs'))
-var parse = require('./src/parse')
-var render = require('./src/render')
-
-/**
- * Reads a JS file from the FS, parses it and returns a markdown doc.
- *
- * @summary String -> Markdown
- */
-module.exports = function (file) {
-  return render(parse(file)) //file.map(parse).map(render)
+module.exports = {
+  write: require('./src/write'),
+  parse: require('./src/parse'),
+  render: require('./src/render'),
 }

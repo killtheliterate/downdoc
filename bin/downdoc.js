335e9ebda06b8f2003d99bf9b274f34afce7e6ed
talon.poole@gmail.com <talon.poole@gmail.com>
2015-08-14 17:30:36 -0600

    rename

--- bin/downdoc.js
+++ bin/downdoc.js
@@ -6,7 +6,7 @@ var argv = require('minimist')(process.argv.slice(2))
 var folder = argv._[0]
 var out = process.cwd() + '/' + argv._[1]
 
-var downdoc = require('../src/downdoc')
+var downdoc = require('../src/write')
 var parse = require('../src/parse')
 var render = require('../src/render')
 

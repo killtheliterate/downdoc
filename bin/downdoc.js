01b00c25b43f8155a41b6cc3814576520be4e4b1
tpoole <tpoole@placeable.com>
2015-08-21 16:50:49 -0600

    need to PR parse-comment for es6

--- bin/downdoc.js
+++ bin/downdoc.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
-var fs = require('vinyl-fs')
-var map = require('map-stream')
-var extend = require('xtend')
-var rimraf = require('rimraf')
+var fs      = require('vinyl-fs')
+var extend  = require('xtend')
+var rimraf  = require('rimraf')
 var resolve = require('path').resolve
 
 var error = function (message) {
@@ -19,7 +18,7 @@ var usage = function () {
 
 var argv = require('minimist')(process.argv.slice(2))
 var glob = argv._[0]
-var out = argv._[1]
+var out  = argv._[1]
 var help = argv.help || argv.h
 
 
@@ -27,14 +26,12 @@ if (help) { usage(); process.exit(0) }
 if (!glob) error('Please provide a source folder.')
 if (!out) error('Please provide an out folder.')
 
-var parse = require('../util/parse')
 var downdoc = require('..')
 
 rimraf(resolve(out), function (e) {
   if (e) throw e
   fs.src(glob)
-    .pipe(map(parse))
-    .pipe(map(downdoc))
+    .pipe(downdoc())
     .pipe(fs.dest(out))
   console.log(
     '\nDocumentation has been written to `' + resolve(out) + '`\n')
--- docs/downdoc.md
+++ docs/downdoc.md
@@ -1,16 +1,16 @@
 ## `defaultExport :: VinyFile -> ()`
 
+
 Create downdocs docs stream from an AST stream
- 
+
 ```js
 var map = require('map-stream')
 var downdoc = require('downdoc')
-var parse = require('downdocutilparse')
- 
-fs.src('src.js')
-  .pipe(map(parse))
-  .pipe(map(downdoc))
-  .pipe(fs.dest('docs'))
+var parse = require('downdoc/util/parse')
+
+fs.src('src/*.js')
+.pipe(map(parse))
+.pipe(map(downdoc))
+.pipe(fs.dest('docs'))
 ```
- 
- 
+
